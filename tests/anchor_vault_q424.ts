1f37ba8433e02c95eb76d3047b19ea3a5dd7f9ac
ShrinathNR <nr.shrinath@gmail.com>
2024-10-23 16:56:11 +0530

    add test

--- tests/anchor_vault_q424.ts
+++ tests/anchor_vault_q424.ts
@@ -8,9 +8,19 @@ describe("anchor_vault_q424", () => {
 
   const program = anchor.workspace.AnchorVaultQ424 as Program<AnchorVaultQ424>;
 
-  it("Is initialized!", async () => {
+  it("initialize!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+  it("deposit", async () => {
+    // Add your test here.
+    const tx = await program.methods.deposit(new anchor.BN(0.2)).rpc();
+    console.log("Your transaction signature", tx);
+  });
+  it("withdraw", async () => {
+    // Add your test here.
+    const tx = await program.methods.withdraw(new anchor.BN(0.1)).rpc();
     console.log("Your transaction signature", tx);
   });
 });
